250f909bc47d6f136e4d151ffec3aad75297d9f9
Timothy Okooboh <okoobohtimothy@gmail.com>
2020-05-16 07:45:34 +0100

    deploy

---
+++ precache-manifest.0fa2a03c707f1e45db488967ed05dec4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a6e3aa80228fbae267e",
+    "url": "/portfolio/css/app.e51db514.css"
+  },
+  {
+    "revision": "a2ddd401747c8be70588",
+    "url": "/portfolio/css/contact.75481e59.css"
+  },
+  {
+    "revision": "0cfcb8df15e4a5a2eeee",
+    "url": "/portfolio/css/projects.7a5c3b5a.css"
+  },
+  {
+    "revision": "4c53ae97b6a371d4b9b7dc968d8fa6b5",
+    "url": "/portfolio/img/corona.4c53ae97.jpeg"
+  },
+  {
+    "revision": "4f8dc12738917e3af235c4ffc5ec0161",
+    "url": "/portfolio/img/dictionary.4f8dc127.jpg"
+  },
+  {
+    "revision": "e2852089a0dce196fc25d6842d993d01",
+    "url": "/portfolio/img/laughter.e2852089.jpg"
+  },
+  {
+    "revision": "92d38eb0e509fe5a8f7f47d230a2f11d",
+    "url": "/portfolio/img/timothy.92d38eb0.png"
+  },
+  {
+    "revision": "e55901bf5d6f3214c799595a097ed624",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8a6e3aa80228fbae267e",
+    "url": "/portfolio/js/app.5fede08b.js"
+  },
+  {
+    "revision": "1e9eccee214408da6613",
+    "url": "/portfolio/js/chunk-vendors.84b5e83e.js"
+  },
+  {
+    "revision": "a2ddd401747c8be70588",
+    "url": "/portfolio/js/contact.f8a45466.js"
+  },
+  {
+    "revision": "0cfcb8df15e4a5a2eeee",
+    "url": "/portfolio/js/projects.fe0f9422.js"
+  },
+  {
+    "revision": "30c1e3c04c8a8723e84df7a7bb0c1d65",
+    "url": "/portfolio/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/portfolio/robots.txt"
+  }
+]);
